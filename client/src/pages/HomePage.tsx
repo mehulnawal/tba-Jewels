f7f4a80f15a45df9c1f8b222408009bb1831ef3d
Mehul Nawal <mehulnawal29@gmail.com>
2026-06-02 09:48:54 +0530

    banner images changed

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -10,9 +10,9 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import { Link } from "react-router-dom";
 
-import banner1 from '../assets/banner/banner1.png'
-import banner2 from '../assets/banner/banner2.png'
-import banner3 from '../assets/banner/banner3.png'
+import banner1 from '../assets/banner/banner1.WebP'
+import banner2 from '../assets/banner/banner2.WebP'
+import banner3 from '../assets/banner/banner3.WebP'
 
 // import { useLenis } from "../providers/LenisProvider";
 
